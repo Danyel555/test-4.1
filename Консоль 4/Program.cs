52d3f2289da3b9da9a16d6d2d0f2d15bcae8c26b
Danyel555 <113988414+Danyel555@users.noreply.github.com>
2022-11-07 22:05:37 +0300

    Update and rename Consolet4/Program.cs to Консоль 4/Program.cs

---
+++ Консоль 4/Program.cs
@@ -0,0 +1,86 @@
+{
+    internal class Program
+    {
+        static void Master()
+        {
+            List<DaybookEntry> daybookEntries = new List<DaybookEntry>();
+            daybookEntries.Add(new DaybookEntry("Название запись", "запись."));
+            daybookEntries.Add(new DaybookEntry(DateTime.Now.AddDays(9), ""));
+            daybookEntries.Add(new DaybookEntry("Текст записи"));
+            daybookEntries.Add(new DaybookEntry("Полная записи", DateTime.Now.AddDays(9), ""));
+
+            Daybook daybook = new Daybook(daybookEntries); 
+            DaybookMenu menu = new DaybookMenu();
+            DateTime selDate = DateTime.Today; int selEntry = 0;
+            while (true)
+            {
+                menu.Update(daybook, selDate, selEntry);
+                menu.Draw();
+                ConsoleKeyInfo ki = Console.ReadKey(true);
+                switch (ki.Key)
+                {
+                    case ConsoleKey.Escape:
+                        return;
+
+                    case ConsoleKey.Enter:
+                        if (selEntry <= daybook.entries.Count)
+                            menu.ShowSelectedEntry(daybook, selEntry);
+                        Console.ReadKey();
+                        break;
+
+                    case ConsoleKey.LeftArrow:
+                        selDate = selDate.AddDays(-1);
+                        break;
+                    case ConsoleKey.RightArrow:
+                        selDate = selDate.AddDays(1);
+                        break;
+                    case ConsoleKey.D:  
+                        try
+                        {
+                            selDate = Convert.ToDateTime(Console.ReadLine());
+                        }
+                        catch
+                        {
+                            Console.WriteLine("Неправильная дата!");
+                        }
+                        break;
+
+                    case ConsoleKey.UpArrow:
+                        if (selEntry > 0)
+                            selEntry -= 1;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (selEntry + 1 < daybook.entries.Count)
+                            selEntry += 1;
+                        break;
+                        // case ConsoleKey.N: 
+                        //     int tmp;
+                        //     try {
+                        //         tmp = Convert.ToInt32(Console.ReadLine());
+                        //     } catch {
+                        //         Console.WriteLine("Введите номер записи!");
+                        //     }
+                        //     break;
+                }
+            }
+        }
+    }
+
+    internal class DaybookMenu
+    {
+        public DaybookMenu()
+        {
+        }
+    }
+
+    internal class Daybook
+    {
+        private List<DaybookEntry> daybookEntries;
+
+        public Daybook(List<DaybookEntry> daybookEntries)
+        {
+            this.daybookEntries = daybookEntries;
+        }
+    }
+}
+
